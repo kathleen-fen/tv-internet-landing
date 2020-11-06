6c68b3ce1108dd3b1e88b856a3e5b2d572540f3d
Kathreen Fenchenko <fev3@mail.ru>
2020-11-06 15:28:35 +0100

    dropdown menu alive

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,26 @@
-$(document).ready(function(){
-    $('.internet__film-carousel').slick({
-        speed: 500,
-        fade: true,
-        cssEase: 'linear',
-        arrows: false,
-        autoplay: true,
-        dots: true,
-        dotsClass: 'dots',
-        appendDots: $('.internet__buy')
-    });
+$(document).ready(function () {
+  $(".internet__film-carousel").slick({
+    speed: 500,
+    fade: true,
+    cssEase: "linear",
+    arrows: false,
+    autoplay: true,
+    dots: true,
+    dotsClass: "dots",
+    appendDots: $(".internet__buy"),
   });
-  
+
+  $(".internet__video-carousel").slick({
+    speed: 500,
+    cssEase: "linear",
+    arrows: true,
+    prevArrow:
+      '<div class="prevArrow"><img src="./img/internet/arrow-left.png" alt=""></div>',
+    nextArrow:
+      '<div class="nextArrow"><img src="./img/internet/arrow-right.png" alt=""></div>',
+  });
+
+  $(".logo__icon").click(function () {
+    $(".dd-menu").toggleClass("dd-menu_open");
+  });
+});
